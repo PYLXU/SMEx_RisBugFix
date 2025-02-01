c8a4bda00bb44fc020eb8c0f150e99111fb65f06
pylxu <e-pylxu@outlook.com>
2025-02-01 19:14:05 +0800

    更新歌词功能，添加歌词和翻译的POST请求，修改扩展包信息

--- function.js
+++ function.js
@@ -1,23 +1,32 @@
-const audioElement = document.getElementById('audio');
-if (audioElement) {
-  const observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-      if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
-        const src = mutation.target.getAttribute('src');
-        // 替换反斜杠为正斜杠，以便进行URL编码
-        const forwardSlashesSrc = src.replace(/\\/g, '/');
-        // 如果src包含#，则进行URL编码
-        if (forwardSlashesSrc.includes('#')) {
-          const encodedSrc = encodeURIComponent(forwardSlashesSrc);
-          // 将编码后的URL中的%2F替换为正斜杠，以保持URL格式
-          const finalSrc = 'file:///' + encodedSrc.replace(/%2F/g, '/').replace(/%3a/g, ':');
-          // 更新audio标签的src属性
-          mutation.target.setAttribute('src', finalSrc);
-        }
-      }
-    });
-  });
+const updateLyrics = () => {
+  const currentLyricsElement = document.querySelector('.SimLRC>.active');
+  const currentLyrics = currentLyricsElement ? currentLyricsElement.firstChild.innerHTML : '';
+  const currentLyricsTranslate = currentLyricsElement && currentLyricsTranslate !== currentLyricsElement.lastChild.innerHTML ? currentLyricsElement.lastChild.innerHTML : '';
 
-  const config = { attributes: true };
-  observer.observe(audioElement, config);
+  console.log('Lyrics:', currentLyrics);
+  console.log('Translation:', currentLyricsTranslate);
+
+  fetch(
+    `http://127.0.0.1:50063/component/lyrics/lyrics/`,
+    {
+        method: "POST",
+        body: JSON.stringify({
+          basic: currentLyrics,
+          extra: currentLyricsTranslate
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+};
+
+const parentElement = document.querySelector('.SimLRC');
+if (parentElement) {
+  const observer = new MutationObserver(updateLyrics);
+  observer.observe(parentElement, { childList: true, subtree: true });
+  updateLyrics();
 }
